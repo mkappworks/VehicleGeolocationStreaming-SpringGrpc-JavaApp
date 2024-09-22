79a0838ad7e69ed55212ee19042c8c8aec9ff0df
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-22 22:28:28 +0200

    Remove unused import

--- consumer-service/src/main/java/com/mkappworks/VehicleGeoLocationController.java
+++ consumer-service/src/main/java/com/mkappworks/VehicleGeoLocationController.java
@@ -4,7 +4,6 @@ import com.google.protobuf.Descriptors;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
