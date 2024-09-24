8e9d5fb8df5c59af43990c1c7b9c9ccbac4a24f8
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-24 18:45:19 +0200

    Added test to VehicleGeoLocationController.getGeoLocationsByVehicle

--- consumer-service/src/main/java/com/mkappworks/controllers/VehicleGeoLocationController.java
+++ consumer-service/src/main/java/com/mkappworks/controllers/VehicleGeoLocationController.java
@@ -3,6 +3,7 @@ package com.mkappworks.controllers;
 import com.mkappworks.dtos.VehicleIdsRequest;
 import com.mkappworks.services.VehicleGeoLocationConsumerService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +16,7 @@ public class VehicleGeoLocationController {
     private final VehicleGeoLocationConsumerService vehicleGeoLocationConsumerService;
 
     @PostMapping("vehicle/geolocation")
-    public List<Map<String, List<Map<String, Object>>>> getGeoLocationsByVehicle(@RequestBody VehicleIdsRequest vehicleIds) {
-        return vehicleGeoLocationConsumerService.getGeoLocationsByVehicle(vehicleIds.ids());
+    public ResponseEntity<List<Map<String, List<Map<String, Object>>>>> getGeoLocationsByVehicle(@RequestBody VehicleIdsRequest vehicleIds) {
+        return ResponseEntity.ok(vehicleGeoLocationConsumerService.getGeoLocationsByVehicle(vehicleIds.ids()));
     }
 }
