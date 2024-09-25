e6c0bc8960bbd3a14ca83ef68b98331d78ac5915
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-25 21:01:33 +0200

    Added ServiceExceptionHandler

--- producer-service/src/main/java/com/mkappworks/ProducerServiceApplication.java
+++ producer-service/src/main/java/com/mkappworks/ProducerServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProducerServiceApplication {
-
     public static void main(final String[] args) {
         SpringApplication.run(ProducerServiceApplication.class, args);
     }
--- producer-service/src/main/java/com/mkappworks/service/advice/ServiceExceptionHandler.java
+++ producer-service/src/main/java/com/mkappworks/service/advice/ServiceExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.mkappworks.service.advice;
 
-import com.mkappworks.exceptions.StreamObserverException;
+import com.mkappworks.exceptions.NoGeoLocationException;
+import com.mkappworks.exceptions.InternalStatusRunTimeException;
+import com.mkappworks.exceptions.UnKnownStatusRunTimeException;
 import io.grpc.Status;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
@@ -8,9 +10,18 @@ import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
 @GrpcAdvice
 public class ServiceExceptionHandler {
 
-    @GrpcExceptionHandler(StreamObserverException.class)
-    public Status handleStreamObserverException(StreamObserverException e) {
-        return Status.ABORTED.withDescription(e.getMessage()).withCause(e);
+    @GrpcExceptionHandler(InternalStatusRunTimeException.class)
+    public Status handleInternalStatusRunTimeException(InternalStatusRunTimeException e) {
+        return Status.INTERNAL.withDescription(e.getMessage());
     }
 
+    @GrpcExceptionHandler(UnKnownStatusRunTimeException.class)
+    public Status handleStreamObserverException(UnKnownStatusRunTimeException e) {
+        return Status.UNKNOWN.withDescription(e.getMessage());
+    }
+
+    @GrpcExceptionHandler(NoGeoLocationException.class)
+    public Status handleNoGeoLocationException(NoGeoLocationException e) {
+        return Status.NOT_FOUND.withDescription(e.getMessage());
+    }
 }
