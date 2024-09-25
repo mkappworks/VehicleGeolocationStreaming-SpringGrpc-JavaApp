2f21c093b5e0a3847d8465df03baca0601fd8e93
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-25 21:04:42 +0200

    Add consumer-service GeoLocationSubscriptionInitializer

---
+++ consumer-service/src/main/java/com/mkappworks/service/GeoLocationSubscriptionInitializer.java
@@ -0,0 +1,25 @@
+package com.mkappworks.service;
+
+import com.mkappworks.proto.Vehicle;
+import com.mkappworks.proto.VehicleGeoLocationServiceGrpc;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GeoLocationSubscriptionInitializer implements CommandLineRunner {
+
+    @GrpcClient("producer-service")
+    private VehicleGeoLocationServiceGrpc.VehicleGeoLocationServiceStub locationClient;
+
+    private final GeoLocationListener listener;
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.locationClient
+                .withWaitForReady()
+                .getGeoLocationsByVehicle(Vehicle.newBuilder().setVehicleId("1").build(), listener);
+    }
+}
