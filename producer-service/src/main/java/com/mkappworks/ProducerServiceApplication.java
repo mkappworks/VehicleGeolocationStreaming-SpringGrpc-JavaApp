a323496b54829434beed41cfd259799e878e0bd3
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-25 15:54:41 +0200

    Removed commandLineRunner from ProducerServiceApplication

--- producer-service/src/main/java/com/mkappworks/ProducerServiceApplication.java
+++ producer-service/src/main/java/com/mkappworks/ProducerServiceApplication.java
@@ -1,20 +1,12 @@
 package com.mkappworks;
 
-import io.grpc.Server;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProducerServiceApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(ProducerServiceApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(final Server server) {
-        return args -> server.awaitTermination();
     }
 }
