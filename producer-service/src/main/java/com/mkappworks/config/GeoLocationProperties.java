be5740939841cb110856642ccbef4aa451d79086
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-29 18:45:07 +0200

    Added GeoLocationProperties

---
+++ producer-service/src/main/java/com/mkappworks/config/GeoLocationProperties.java
@@ -0,0 +1,16 @@
+package com.mkappworks.config;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "geo-location")
+@Setter
+public class GeoLocationProperties {
+    private int waitTime;
+
+    public int getWaitTime() {
+        return waitTime == 0 ? 2 : waitTime;
+    }
+}
