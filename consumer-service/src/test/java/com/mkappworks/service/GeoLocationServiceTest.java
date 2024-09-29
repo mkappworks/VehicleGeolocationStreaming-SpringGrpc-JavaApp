c4c391edb3f858ea03d219c1758a537e9ad96444
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-29 19:25:00 +0200

    Added GeoLocationServiceTest

---
+++ consumer-service/src/test/java/com/mkappworks/service/GeoLocationServiceTest.java
@@ -0,0 +1,77 @@
+package com.mkappworks.service;
+
+import com.mkappworks.proto.GeoLocation;
+import com.mkappworks.proto.ReactorVehicleGeoLocationServiceGrpc;
+import com.mkappworks.proto.Vehicle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+class GeoLocationServiceTest {
+
+    @Mock
+    private ReactorVehicleGeoLocationServiceGrpc.ReactorVehicleGeoLocationServiceStub vehicleGeoLocationServiceStub;
+
+    @InjectMocks
+    private GeoLocationService geoLocationService;
+
+    private Vehicle testVehicle;
+    private GeoLocation geoLocation1;
+    private GeoLocation geoLocation2;
+
+    @BeforeEach
+    void setUp() {
+        testVehicle = Vehicle.newBuilder()
+                .setVehicleId("vehicle123")
+                .build();
+
+        geoLocation1 = GeoLocation.newBuilder()
+                .setVehicleId("vehicle123")
+                .setLongitude(12.34f)
+                .setLatitude(56.78f)
+                .setTimeStamp(1727629161)
+                .build();
+
+        geoLocation2 = GeoLocation.newBuilder()
+                .setVehicleId("vehicle123")
+                .setLongitude(98.76f)
+                .setLatitude(54.32f)
+                .setTimeStamp(1827629161)
+                .build();
+    }
+
+    @Test
+    void getGeoLocations_should_return_flux_of_geo_locations() {
+        Mockito.when(vehicleGeoLocationServiceStub.getGeoLocationsByVehicle(Mockito.any(Mono.class)))
+                .thenReturn(Flux.just(geoLocation1, geoLocation2));
+
+        Flux<GeoLocation> geoLocationFlux = geoLocationService.getGeoLocations(testVehicle);
+
+        // Use StepVerifier to verify the Flux
+        StepVerifier.create(geoLocationFlux)
+                .expectNext(geoLocation1)
+                .expectNext(geoLocation2)
+                .verifyComplete();
+    }
+
+    @Test
+    void getGeoLocations_should_return_empty_flux_if_no_geo_locations() {
+        Mockito.when(vehicleGeoLocationServiceStub.getGeoLocationsByVehicle(Mockito.any(Mono.class)))
+                .thenReturn(Flux.empty());
+
+        Flux<GeoLocation> geoLocationFlux = geoLocationService.getGeoLocations(testVehicle);
+
+        // Verify that the Flux is empty
+        StepVerifier.create(geoLocationFlux)
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+}
